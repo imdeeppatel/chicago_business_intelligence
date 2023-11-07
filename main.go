31463df9069fee0d9fb9a50a23bd6979449ff857
Kishan Patel <kpatel3032@gmail.com>
2023-11-07 12:33:42 -0600

    Added API-KEY

--- main.go
+++ main.go
@@ -252,7 +252,7 @@ func GetTaxiTrips(db *sql.DB) {
 	// Get your geocoder.ApiKey from here :
 	// https://developers.google.com/maps/documentation/geocoding/get-api-key?authuser=2
 
-	geocoder.ApiKey = "ADD-YOUR-API-KEY-HERE"
+	geocoder.ApiKey = "AIzaSyCfozmFdVlVJuvuuNGVdsnvWw2Ig5-mVyI"
 
 	
 
